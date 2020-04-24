020ea8cdd4b2cf87f310cbecaed207ffae63901d
Lera <valeriya.mihaleva.1999@gmail.com>
2020-04-24 19:10:56 +0300

    [functions] Add sendPushNotification on creating tweet

--- packages/functions/index.js
+++ packages/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions')
+const request = require('request')
 
 exports.onTweetCreate = functions.database
   .ref('/tweets/{tweetId}')
@@ -6,4 +7,31 @@ exports.onTweetCreate = functions.database
     const tweet = snapshot.val()
 
     console.log('new tweet', JSON.stringify(tweet))
+
+    return sendPushNotification()
   })
+
+async function sendPushNotification() {
+  const expoToken = 'ExponentPushToken[dhKlfhNfhz19WBbEc3R3fp]'
+
+  const message = {
+    to: expoToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { data: 'goes here' },
+    _displayInForeground: true,
+  }
+  const response = await request({
+    method: 'POST',
+    uri: 'https://exp.host/--/api/v2/push/send',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    json: message,
+  })
+
+  console.log(JSON.stringify(response))
+}
